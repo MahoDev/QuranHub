ce7130dce7632749f3f155d6f2df29c73c5f5574
MahoDev <mt.alshahat@gmail.com>
2025-03-21 08:38:04 +0200

    Fixed middleware bug causing sitemap and robots to be unreachable

--- middleware.js
+++ middleware.js
@@ -67,6 +67,5 @@ export default async function middleware(req) {
 }
 
 export const config = {
-	matcher:
-		"/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)",
+	matcher: "/(.*)",
 };
